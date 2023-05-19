7e6cc3418eb78fc0289f8efe0e5c2afcbbaaf488
Nnamdi4tech <122917052+Nnamdi4tech@users.noreply.github.com>
2023-05-19 11:03:07 -0700

    Add files via upload

---
+++ JQ.js
@@ -0,0 +1,24 @@
+$("document").ready(function(){
+    // THis document above, is refer to all document in this JQ. Make use of the ready in your javascript and Jquery.
+
+
+
+
+//  $("#date").datepicker();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
